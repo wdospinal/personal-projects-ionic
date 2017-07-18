7ceec485e5bb792fd4876573b90e3034f2525f7d
wdospinal <wdospinal@unal.edu.co>
2017-07-18 15:23:52 -0500

    Add like method

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
+import { Project } from '../../model/project';
 import { ProjectComponent } from '../project/project.component';
 
 @Component({
@@ -17,5 +18,13 @@ export class HomePage {
         limitToLast: 50
       }
     });
+  }
+  changeLike(event) {
+    const likes = event.likes + 1;
+    this.items.update(event.$key, { likes: likes });
+  }
+
+  updateList(project: Project) {
+    this.items.push(project);
   }
 }
